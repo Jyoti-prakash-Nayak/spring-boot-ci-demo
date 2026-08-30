b7fecc40add760447d001d7aff795f67d30524ef
Jyoti-prakash-Nayak <87771215+Jyoti-prakash-Nayak@users.noreply.github.com>
2026-08-30 22:18:34 +0530

    update fix

--- src/main/java/hello_app/hello_app/HelloController.java
+++ src/main/java/hello_app/hello_app/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello CI/CD Pipeline....!";
+        return "Hello CI/CD Pipeline....!!";
     }
 }
