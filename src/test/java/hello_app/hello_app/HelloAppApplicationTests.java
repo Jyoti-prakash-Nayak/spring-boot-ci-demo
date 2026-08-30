5ef781bc39e64beb844a8c5ad373ed9d56725231
Jyoti-prakash-Nayak <87771215+Jyoti-prakash-Nayak@users.noreply.github.com>
2026-08-30 19:05:21 +0530

    update

---
+++ src/test/java/hello_app/hello_app/HelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hello_app.hello_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
